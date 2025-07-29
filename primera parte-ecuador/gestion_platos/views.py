783b3a5ee467dde6c38dac02611fba5136276edc
davidMoreno16 <cdmoreno3@utpl.edu.ec>
2025-07-28 23:41:39 -0500

    Update views.py

--- primera parte-ecuador/gestion_platos/views.py
+++ primera parte-ecuador/gestion_platos/views.py
@@ -1,1 +1,6 @@
+from django.shortcuts import render
+from .models import PlatoTipico
 
+def lista_platos(request):
+    platos = PlatoTipico.objects.all()
+    return render(request, 'lista_platos.html', {'platos': platos})
